80d7c4e18942dfd4cd0e9c1c08bf6344c22f49c8
OliverWales <oliverfwales@gmail.com>
2020-11-04 12:03:15 +0000

    Implement framerate counter

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Renderer2D } from "./Renderer2D";
 import { BasicAgent } from "./BasicAgent";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const framerate = document.getElementById("framerate") as HTMLCanvasElement;
 const playButton = document.getElementById("playButton") as HTMLButtonElement;
 const stepButton = document.getElementById("stepButton") as HTMLButtonElement;
 
@@ -16,6 +17,8 @@ export function init() {
   simulation.addAgent(agent);
 
   let lastRender = 0;
+  let lastFPS = 0;
+  let frames = 0;
 
   function loop(timestamp: number) {
     let deltaT = timestamp - lastRender;
@@ -28,6 +31,17 @@ export function init() {
 
     // render
     simulation.draw();
+    frames++;
+
+    // every 250ms, recalculate framerate
+    if (timestamp - lastFPS >= 250) {
+      framerate.textContent = `FPS: ${(
+        (1000 * frames) /
+        (timestamp - lastFPS)
+      ).toFixed(1)}`;
+      frames = 0;
+      lastFPS = timestamp;
+    }
 
     window.requestAnimationFrame(loop);
   }
