3c6c61d1c891e1cec7bd761428ba98e7e22541fb
OliverWales <oliverfwales@gmail.com>
2020-12-12 11:28:13 +0000

    Complete VO implementation

--- src/VOAgent.ts
+++ src/VOAgent.ts
@@ -4,8 +4,8 @@ import { Vector2f } from "./Vector2f";
 interface VelocityObstacle {
   // Velocity object defined by a point and two direction vectors
   vertex: Vector2f;
-  t1: Vector2f;
-  t2: Vector2f;
+  tangent1: Vector2f;
+  tangent2: Vector2f;
 }
 
 export class VOAgent implements IAgent {
@@ -59,15 +59,13 @@ export class VOAgent implements IAgent {
   }
 
   update(deltaT: number, neighbours: IAgent[]): void {
-    this._isStuck = false;
-
-    let speed = 0.5;
-
     if (this._isDone) {
       return;
     }
 
-    let preferredVelocity = this.getPreferredVelocity().multiply(speed);
+    this._isStuck = false;
+    let speed = 0.3;
+    let preferredVelocity = this.getPreferredVelocity(speed);
 
     let safe = true;
     let collision;
@@ -98,55 +96,111 @@ export class VOAgent implements IAgent {
 
     if (collision != null) {
       // Else, consider closest point on first half-plane
-      let hp1 = this.getClosestPointOnLine(
+      let halfplane1 = this.getClosestPointOnLine(
         collision.vertex,
-        collision.t1,
+        collision.tangent1,
         preferredVelocity
       );
-      safe = true;
-      for (var i = 0; i < neighbours.length; i++) {
-        if (i != agent) {
-          let VO = this.getVelocityObstacle(neighbours[i]);
-          if (VO == null || this.isInside(hp1, VO)) {
-            safe = false;
-            break;
+
+      if (halfplane1.magnitude() <= speed) {
+        safe = true;
+        for (var i = 0; i < neighbours.length; i++) {
+          if (i != agent) {
+            let velocityObstacle = this.getVelocityObstacle(neighbours[i]);
+            if (
+              velocityObstacle == null ||
+              this.isInside(halfplane1, velocityObstacle)
+            ) {
+              safe = false;
+              break;
+            }
           }
         }
-      }
 
-      if (safe) {
-        this._nextDirection = hp1;
-        this._nextPosition = this._position.add(hp1);
-        return;
+        if (safe) {
+          this._nextDirection = halfplane1;
+          this._nextPosition = this._position.add(halfplane1);
+          return;
+        }
       }
 
       // Else, consider closest point on second half-plane
-      let hp2 = this.getClosestPointOnLine(
+      let halfPlane2 = this.getClosestPointOnLine(
         collision.vertex,
-        collision.t2,
+        collision.tangent2,
         preferredVelocity
       );
-      safe = true;
-      for (var i = 0; i < neighbours.length; i++) {
-        if (i != agent) {
-          let VO = this.getVelocityObstacle(neighbours[i]);
-          if (VO == null || this.isInside(hp2, VO)) {
-            safe = false;
-            break;
+
+      if (halfplane1.magnitude() <= speed) {
+        safe = true;
+        for (var i = 0; i < neighbours.length; i++) {
+          if (i != agent) {
+            let velocityObstacle = this.getVelocityObstacle(neighbours[i]);
+            if (
+              velocityObstacle == null ||
+              this.isInside(halfPlane2, velocityObstacle)
+            ) {
+              safe = false;
+              break;
+            }
+          }
+        }
+
+        if (safe) {
+          this._nextDirection = halfPlane2;
+          this._nextPosition = this._position.add(halfPlane2);
+          return;
+        }
+      }
+    }
+
+    // Else, sample random velocities and select the one with the least penalty
+    let samples = 100; // number of velocities to try
+    let w = 100; // parameter for penalty
+    let minPenalty = Infinity;
+    let bestVelocity = new Vector2f(0, 0);
+
+    for (var i = 0; i < samples; i++) {
+      // Sample velocity from entire allowed velocity space
+      let sample = new Vector2f(0, 0).sample(speed);
+      let minTimeToCollision = Infinity;
+
+      // Find time to first collision
+      for (var j = 0; j < neighbours.length; j++) {
+        let b = neighbours[j];
+        let VO = this.getVelocityObstacle(b);
+        if (VO == null || this.isInside(sample, VO)) {
+          let timeToCollision = this.getFirstRayCircleIntersection(
+            b.getPosition(),
+            this.Radius + b.Radius,
+            this._position,
+            sample
+          );
+
+          if (timeToCollision < minTimeToCollision) {
+            minTimeToCollision = timeToCollision;
+            if (minTimeToCollision == 0) {
+              return;
+            }
           }
         }
       }
 
-      if (safe) {
-        this._nextDirection = hp2;
-        this._nextPosition = this._position.add(hp2);
-        return;
+      // Calculate penalty
+      let penalty =
+        w / minTimeToCollision + preferredVelocity.subtract(sample).magnitude(); // TS correctly handles divide by zero or infinity
+      if (penalty < minPenalty) {
+        minPenalty = penalty;
+        bestVelocity = sample;
       }
     }
 
-    // TODO: Else, sample
-    this._nextDirection = new Vector2f(0, 0);
-    this._isStuck = true;
+    if (bestVelocity.magnitude() == 0) {
+      this._isStuck = true;
+    }
+
+    this._nextDirection = bestVelocity;
+    this._nextPosition = this._position.add(bestVelocity);
     return;
   }
 
@@ -155,66 +209,103 @@ export class VOAgent implements IAgent {
     this._position = this._nextPosition;
   }
 
-  private getPreferredVelocity(): Vector2f {
+  private getPreferredVelocity(maxSpeed: number): Vector2f {
     let goalDirection = this._goalPosition.subtract(this._position);
     let goalDistance = goalDirection.magnitude();
 
-    if (goalDistance > 1) {
-      return goalDirection.divide(goalDistance);
+    if (goalDistance > maxSpeed) {
+      return goalDirection.divide(goalDistance / maxSpeed);
     }
     return goalDirection;
   }
 
   private getVelocityObstacle(b: IAgent): VelocityObstacle | null {
-    let vB = b.getDirection();
-    let R = b.Radius + this.Radius; // Radius of Minkowski sum
+    let velocityB = b.getDirection();
 
-    // Translate positions as if P_A = (0, 0)
-    let pB = b.getPosition().subtract(this.getPosition());
+    // Translate origin to this agent's position
+    let positionB = b.getPosition().subtract(this._position);
 
-    // Find centre of Minkowski sum
-    let pC = pB.add(vB);
+    // Find Minkowski sum of agents
+    let centre = positionB.add(velocityB);
+    let radius = b.Radius + this.Radius;
 
     // Calculate angles
-    let diff = vB.subtract(pC);
+    let diff = velocityB.subtract(centre);
     let dist = diff.magnitude();
-    if (dist < R) {
+    if (dist < radius) {
       return null;
     }
 
-    let t = Math.acos(R / dist);
-    let d = Math.atan2(diff.y, diff.x);
+    let theta = Math.acos(radius / dist);
+    let phi = Math.atan2(diff.y, diff.x);
 
     // Calculate tangent vectors
-    let a1 = d + t;
-    let t1 = new Vector2f(pC.x + R * Math.cos(a1), pC.y + R * Math.sin(a1));
+    let angle1 = phi + theta;
+    let tangent1 = new Vector2f(
+      centre.x + radius * Math.cos(angle1),
+      centre.y + radius * Math.sin(angle1)
+    );
 
-    let a2 = d - t;
-    let t2 = new Vector2f(pC.x + R * Math.cos(a2), pC.y + R * Math.sin(a2));
+    let angle2 = phi - theta;
+    let tangent2 = new Vector2f(
+      centre.x + radius * Math.cos(angle2),
+      centre.y + radius * Math.sin(angle2)
+    );
 
     // Return velocity obstacle
-    return { vertex: vB, t1: t1, t2: t2 };
+    return { vertex: velocityB, tangent1: tangent1, tangent2: tangent2 };
   }
 
   private isInside(velocity: Vector2f, VO: VelocityObstacle): boolean {
-    let halfPlane1 =
-      (VO.t1.x - VO.vertex.x) * (velocity.y - VO.vertex.y) -
-      (VO.t1.y - VO.vertex.y) * (velocity.x - VO.vertex.x);
-    let halfPlane2 =
-      (VO.t2.x - VO.vertex.x) * (velocity.y - VO.vertex.y) -
-      (VO.t2.y - VO.vertex.y) * (velocity.x - VO.vertex.x);
+    // First half-plane
+    let determinant1 =
+      (VO.tangent1.x - VO.vertex.x) * (velocity.y - VO.vertex.y) -
+      (VO.tangent1.y - VO.vertex.y) * (velocity.x - VO.vertex.x);
 
-    return halfPlane1 > 0 && halfPlane2 < 0;
+    // Second half-plane
+    let determinant2 =
+      (VO.tangent2.x - VO.vertex.x) * (velocity.y - VO.vertex.y) -
+      (VO.tangent2.y - VO.vertex.y) * (velocity.x - VO.vertex.x);
+
+    return determinant1 > 0 && determinant2 < 0;
   }
 
   private getClosestPointOnLine(
     linePoint: Vector2f,
     lineDirection: Vector2f,
     point: Vector2f
-  ) {
+  ): Vector2f {
     let direction = lineDirection.normalise();
-    let v = point.subtract(linePoint);
-    let d = v.dot(direction);
-    return linePoint.add(direction.multiply(d));
+    let vector = point.subtract(linePoint);
+    let distance = vector.dot(direction);
+    return linePoint.add(direction.multiply(distance));
+  }
+
+  private getFirstRayCircleIntersection(
+    centre: Vector2f,
+    radius: number,
+    origin: Vector2f,
+    direction: Vector2f
+  ): number {
+    let delta = origin.subtract(centre);
+
+    let discrim =
+      direction.dot(delta) ** 2 -
+      direction.dot(direction) * (delta.magnitudeSqrd() - radius ** 2);
+
+    if (discrim < 0) {
+      // No intersection
+      return Infinity;
+    }
+
+    let distance =
+      (-direction.dot(delta) - Math.sqrt(discrim)) / direction.magnitudeSqrd();
+
+    if (distance < 0) {
+      // Intersection behind
+      return Infinity;
+    }
+
+    return distance;
   }
 }
