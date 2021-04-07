6f05d6a0876c9ad852b84d51f928da8029efae60
OliverWales <oliverfwales@gmail.com>
2021-04-07 10:53:37 +0100

    Add timing to empty logger

--- src/loggers/VacuousLogger.ts
+++ src/loggers/VacuousLogger.ts
@@ -1,14 +1,23 @@
+import { IAgentCollection } from "../IAgentCollection";
 import { ILogger } from "../ILogger";
+import { IObstacle } from "../IObstacle";
 
 // Empty logger implementation to allow measurement of the probe effect
 export class VacuousLogger implements ILogger {
   private _logging: boolean = false;
+  private _totalTime: number = 0;
 
   start() {
     console.log("Started logging");
   }
 
-  log(): void {}
+  log(
+    _agents: IAgentCollection,
+    _obstacles: IObstacle[],
+    deltaT: number
+  ): void {
+    this._totalTime += deltaT;
+  }
 
   stop(): void {
     if (!this._logging) {
@@ -16,6 +25,7 @@ export class VacuousLogger implements ILogger {
     }
 
     console.log("Stopped logging");
+    console.log(`Total time: ${this._totalTime.toFixed(3)} seconds`);
     this._logging = false;
   }
 }
