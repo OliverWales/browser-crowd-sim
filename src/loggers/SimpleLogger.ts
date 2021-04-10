69e2d7aa5f3c4d96bed2e24aba22bab32250d1a3
OliverWales <oliverfwales@gmail.com>
2021-04-10 23:14:07 +0100

    Add quartile calculation for box plots

--- src/loggers/SimpleLogger.ts
+++ src/loggers/SimpleLogger.ts
@@ -31,8 +31,6 @@ export class SimpleLogger implements ILogger {
       this._pathLengths[agent.Id] = 0;
       this._startPositions[agent.Id] = agent.getPosition();
     });
-
-    console.log("Started logging");
   }
 
   log(agents: IAgentCollection, obstacles: IObstacle[], deltaT: number): void {
@@ -94,13 +92,14 @@ export class SimpleLogger implements ILogger {
       return;
     }
 
-    console.log("Stopped logging");
     console.log(
-      `Total time: ${(performance.now() - this._startTime).toFixed(3)} ms`
+      `Total time (ms): ${(performance.now() - this._startTime).toFixed(3)}`
     );
     this._logging = false;
 
     const totalFrameTime = this.sum(this._frameTimes);
+    const averageFrameTime = totalFrameTime / this._timeStep;
+    const frameTimeQuartiles = this.quartiles(this._frameTimes);
     const totalAgentCollisions = this.sum(this._agentCollisions);
     const totalObstacleCollisions = this.sum(this._obstacleCollisions);
     const overhead: number[] = [];
@@ -114,10 +113,17 @@ export class SimpleLogger implements ILogger {
         1;
     });
     const averageOverhead = this.sum(overhead) / agents.getAll().length;
+    const overheadQuartiles = this.quartiles(overhead.slice(1));
 
     console.log(`Timesteps: ${this._timeStep}`);
-    console.log(`Frame time: ${totalFrameTime.toFixed(3)} ms`);
-    console.log(`Frame times: ${this._frameTimes}`);
+    console.log(`Total frame time (ms): ${totalFrameTime.toFixed(3)}`);
+    console.log(`Average frame time (ms): ${averageFrameTime.toFixed(3)}`);
+    console.log(`Min: ${frameTimeQuartiles.minimum}`);
+    console.log(` LQ: ${frameTimeQuartiles.lowerQuart}`);
+    console.log(`Med: ${frameTimeQuartiles.median}`);
+    console.log(` UQ: ${frameTimeQuartiles.upperQuart}`);
+    console.log(`Max: ${frameTimeQuartiles.maximum}`);
+    //console.log(`Frame times: ${this._frameTimes}`);
 
     console.log(`Total agent collisions: ${totalAgentCollisions}`);
     if (totalAgentCollisions > 0) {
@@ -126,7 +132,7 @@ export class SimpleLogger implements ILogger {
           totalAgentCollisions / this._timeStep
         ).toFixed(3)}`
       );
-      console.log(`Agent collisions:\n${this._agentCollisions}`);
+      //console.log(`Agent collisions:\n${this._agentCollisions}`);
     }
 
     console.log(`Total obstacle collisions: ${totalObstacleCollisions}`);
@@ -136,11 +142,16 @@ export class SimpleLogger implements ILogger {
           totalObstacleCollisions / this._timeStep
         ).toFixed(3)}`
       );
-      console.log(`Obstacle collisions:\n${this._obstacleCollisions}`);
+      //console.log(`Obstacle collisions:\n${this._obstacleCollisions}`);
     }
 
     console.log(`Average overhead: ${averageOverhead}`);
-    console.log(`Overheads:\n${overhead}`);
+    console.log(`Min: ${overheadQuartiles.minimum}`);
+    console.log(` LQ: ${overheadQuartiles.lowerQuart}`);
+    console.log(`Med: ${overheadQuartiles.median}`);
+    console.log(` UQ: ${overheadQuartiles.upperQuart}`);
+    console.log(`Max: ${overheadQuartiles.maximum}`);
+    //console.log(`Overheads:\n${overhead}`);
   }
 
   round3dp(f: number): number {
@@ -149,5 +160,22 @@ export class SimpleLogger implements ILogger {
 
   sum(arr: number[]): number {
     return arr.reduce((a, b) => a + b, 0);
+  }
+
+  quartiles(arr: number[]) {
+    const sorted = arr.slice().sort((a, b) => a - b);
+    const n = sorted.length;
+
+    const mid = Math.floor(n / 2);
+    const median =
+      n % 2 == 0 ? (sorted[mid] + sorted[mid + 1]) / 2 : sorted[mid];
+
+    return {
+      minimum: sorted[0],
+      lowerQuart: sorted[Math.floor(n * 0.25) - 1],
+      median: median,
+      upperQuart: sorted[Math.floor(n * 0.75) - 1],
+      maximum: sorted[n - 1],
+    };
   }
 }
