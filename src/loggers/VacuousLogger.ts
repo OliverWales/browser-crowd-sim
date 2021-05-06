2d71065beccb0da4841e5128f2c9075d0e6199ff
OliverWales <oliverfwales@gmail.com>
2021-05-06 10:52:02 +0100

    Tweak vacuous logger implementation to better match the simple logger

--- src/loggers/VacuousLogger.ts
+++ src/loggers/VacuousLogger.ts
@@ -6,9 +6,13 @@ import { IObstacle } from "../interfaces/IObstacle";
 export class VacuousLogger implements ILogger {
   private _logging: boolean = false;
   private _totalTime: number = 0;
+  private _frames: number = 0;
 
-  start() {
-    console.log("Started logging");
+  start(_agents: IAgentCollection) {
+    console.log("------- Started logging -------");
+    this._logging = true;
+    this._totalTime = 0;
+    this._frames = 0;
   }
 
   log(
@@ -17,16 +21,25 @@ export class VacuousLogger implements ILogger {
     _stepSize: number,
     deltaT: number
   ): void {
+    if (!this._logging) {
+      return;
+    }
+
     this._totalTime += deltaT;
+    this._frames++;
   }
 
-  stop(): void {
+  stop(_agents: IAgentCollection): void {
     if (!this._logging) {
       return;
     }
 
     console.log("Stopped logging");
-    console.log(`Total time: ${this._totalTime.toFixed(3)} seconds`);
+    console.log(`Total time (ms): ${this._totalTime.toFixed(3)}`);
+    this._logging = false;
+    console.log(`Frames: ${this._frames}`);
+    this._logging = false;
+    console.log(`ms / Frame: ${(this._totalTime / this._frames).toFixed(3)}`);
     this._logging = false;
   }
 }
